b48c52ab191837fda55c992e529f932aed15af2f
Timilehin Oshin <tdoshin@gmail.com>
2021-05-06 20:00:04 -0500

    added Router to indexjs file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import router
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <Router>
